23e84ed146f63cc28367c3f96e5bb745ff172e1b
ruveer <81567814+ruveer@users.noreply.github.com>
2022-05-16 16:06:46 +0530

    Add files via upload

---
+++ main (2).js
@@ -0,0 +1,52 @@
+x = 0;
+y = 0;
+
+draw_apple = "";
+
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
+
+function start()
+{
+  document.getElementById("status").innerHTML = "System is listening please speak";  
+  recognition.start();
+} 
+ 
+recognition.onresult = function(event) {
+
+ console.log(event); 
+ to_number = Number(content);
+ content = event.results[0][0].transcript;
+if(Number.isInteger(to_number)){
+  
+}
+    document.getElementById("status").innerHTML = "The speech has been recognized: " + content; 
+
+}
+
+function preload(){
+  document.getElementById("apple.png").innerHTML= loadImage()
+}
+
+function setup() {
+ 
+}
+
+function draw() {
+  if(draw_apple == "set")
+  {
+    document.getElementById("status").innerHTML = to_number + " Apples drawn";
+    draw_apple = "";
+  }
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    speak_data = "";
+}
